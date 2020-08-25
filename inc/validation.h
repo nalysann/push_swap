a6e06332b2be6a61fe03fa6606ae51a762c014b3
nalysann <urb-ilya@yandex.ru>
2020-08-25 13:46:13 +0300

    added validation

---
+++ inc/validation.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validation.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nalysann <urb-ilya@yandex.ru>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/25 11:19:36 by nalysann          #+#    #+#             */
+/*   Updated: 2020/08/25 11:19:38 by nalysann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VALIDATION_H
+# define VALIDATION_H
+
+# include <stdbool.h>
+
+bool	is_valid_input(int argc, char *argv[], bool verbose);
+
+#endif
---
+++ src/validation.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validation.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nalysann <urb-ilya@yandex.ru>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/25 11:19:49 by nalysann          #+#    #+#             */
+/*   Updated: 2020/08/25 11:19:51 by nalysann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_stdlib.h"
+
+#include <limits.h>
+#include <stdbool.h>
+
+static bool		is_unique(int idx, char *argv[])
+{
+	int		i;
+
+	i = 1;
+	while (i < idx)
+	{
+		if (ft_atoll(argv[i]) == ft_atoll(argv[idx]))
+			return (false);
+		++i;
+	}
+	return (true);
+}
+
+bool			is_valid_input(int argc, char *argv[], bool verbose)
+{
+	int			i;
+	long long	number;
+	char		*endptr;
+
+	i = 1;
+	if (verbose && ft_strequ(argv[1], "-v"))
+		++i;
+	while (i < argc)
+	{
+		number = ft_strtoll(argv[i], &endptr, 10);
+		if (*endptr != '\0' ||
+			!(INT_MIN <= number && number <= INT_MAX) ||
+			!is_unique(i, argv))
+			return (false);
+		++i;
+	}
+	return (true);
+}
